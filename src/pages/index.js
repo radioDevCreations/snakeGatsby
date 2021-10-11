73a7e29a2ffea8cdd3210311c152f229cb894ba3
radioDevCreations <radekrozmus@gmail.com>
2021-10-11 08:52:36 +0200

    snake in gatsby init base

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/style/index.css";
+import SnakeBoard from "../components/SnakeBoard";
+
+const Home = () => {
+  return (
+    <SnakeBoard />
+  );
+}
+
+export default Home;
